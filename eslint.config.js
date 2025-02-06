dd31ef11ed96bc786c464512d947ec4ba5c7f999
futenma <futenma@trymore-inc.jp>
2025-02-07 03:25:06 +0900

    remove: js-beautify&update

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import js from '@eslint/js'
 export default [
   js.configs.recommended,
   {
-    files: ['src/**/*.js'],
+    files: ['src/scripts/**/*.js'],
     languageOptions: {
       parserOptions: {
         parser: '@typescript-eslint/parser',
