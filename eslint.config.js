e56a3f05c0c6a65e1d2d713324c585402b07e8f1
futenma <futenma@trymore-inc.jp>
2025-02-07 10:39:04 +0900

    add: glob

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import js from '@eslint/js'
 export default [
   js.configs.recommended,
   {
-    files: ['src/scripts/**/*.js'],
+    files: ['src/**/*.js'],
     languageOptions: {
       parserOptions: {
         parser: '@typescript-eslint/parser',
